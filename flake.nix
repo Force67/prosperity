8125a13b74fe70418f085d3619dd6bdacb12914f
Vincent Hengel <prelink835@gmail.com>
2026-05-08 21:48:00 +0200

    add nix flake for the dev shell

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "PS4Delta - PS4 emulation and research project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        ps4delta = pkgs.stdenv.mkDerivation {
+          pname = "ps4delta";
+          version = "0.0.0";
+
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+          ];
+
+          # capstone, fmtlib, zlib, xbyak, equilibrium are vendored as
+          # submodules, so we do not bring system copies into the closure.
+          buildInputs = [ ];
+
+          cmakeFlags = [
+            "-GNinja"
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ps4delta $out/bin/
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Early high-level PlayStation 4 emulator";
+            license = licenses.gpl2Plus;
+            platforms = platforms.linux;
+            mainProgram = "ps4delta";
+          };
+        };
+      in {
+        packages.default = ps4delta;
+        packages.ps4delta = ps4delta;
+
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            cmake
+            ninja
+            pkg-config
+            clang_18
+            clang-tools
+            gdb
+            git
+          ];
+
+          shellHook = ''
+            echo "ps4delta dev shell, run:  cmake -G Ninja -B build && ninja -C build"
+          '';
+        };
+      });
+}
